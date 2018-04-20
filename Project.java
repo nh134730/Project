0cd472e15490fc57429d6d3061b5ea27f840487c
Nathan Heindl <nh134730@g.risd.org>
2018-04-20 15:54:46 -0500

    Made the Scanner to imput the data

--- Project.java
+++ Project.java
@@ -3,6 +3,11 @@ import java.util.*;
 import static java.lang.System.*;
 public class Project
 {
+
 	public static void main(String args[]){
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Enter a string :: ");
+		String bobby = keyboard.next();
 	}
 }
